f5d7ecca6c46cbf85d473a24dc077d59f4fe8044
ranjeet kumar <ranjeet.kumar85@t-mobile.com>
2021-06-01 17:26:40 -0700

    Chore -  Undo Implementation

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,3 +1,4 @@
+import { removeFromReadingList } from './../../../../data-access/src/lib/+state/reading-list.actions';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
@@ -8,8 +9,13 @@ import {
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
-import { Book } from '@tmo/shared/models';
+import { Book, ReadingListItem } from '@tmo/shared/models';
 import { debounceTime } from 'rxjs/operators';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'tmo-book-search',
@@ -19,13 +25,18 @@ import { debounceTime } from 'rxjs/operators';
 export class BookSearchComponent implements OnInit {
   books: ReadingListBook[];
 
+  private horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  private verticalPosition: MatSnackBarVerticalPosition = 'top';
+  private duration =  3;
+
   searchForm = this.fb.group({
     term: ''
   });
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private _snackBar: MatSnackBar
   ) {}
 
   get searchTerm(): string {
@@ -48,8 +59,9 @@ export class BookSearchComponent implements OnInit {
       : undefined;
   }
 
-  addBookToReadingList(book: Book) {
-    this.store.dispatch(addToReadingList({ book }));
+  public addBookToReadingList(book: Book) {
+     this.store.dispatch(addToReadingList({ book }));
+    this.openSnackBar( book );
   }
 
   searchExample() {
@@ -64,5 +76,24 @@ export class BookSearchComponent implements OnInit {
       this.store.dispatch(clearSearch());
     }
   }
-  
+
+  private openSnackBar(book: Book ) {
+    let item: ReadingListItem =   this.createBookFromItem( book);
+    this._snackBar.open('Book is added to List.','UNDO', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.duration*1000,
+    }).onAction().subscribe(value => this.store.dispatch(removeFromReadingList({ item })));
+  }
+
+  private createBookFromItem( book: Book) : ReadingListItem{
+    let item = <ReadingListItem>{};
+    item.bookId = book.id;
+    item.authors = book.authors;
+    item.description =  book.description;
+    item.coverUrl =  book.coverUrl;
+    item.publishedDate = book.publishedDate;
+    item.publisher =  book.publisher;
+    return item;
+  }
 }
--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,9 @@
+import { Book } from './../../../../../shared/models/src/models';
 import { Component } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, addToReadingList } from '@tmo/books/data-access';
+import { ReadingListItem } from 'libs/shared/models/src/models';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -8,11 +11,40 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
   styleUrls: ['./reading-list.component.scss']
 })
 export class ReadingListComponent {
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  duration = 3;
+   
   readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private _snackBar: MatSnackBar) {}
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+    this.openSnackBar(item);
+  }
+
+  openSnackBar(item: ReadingListItem ) {
+    let book = <Book>{};
+    this.createBookfromItem(item, book);
+    console.log("book-id "+book.id);
+    this._snackBar.open('Book is removed from List.','UNDO', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration:this.duration * 1000,
+    }).onAction().subscribe(value =>  this.store.dispatch(addToReadingList({ book })) );
+  }
+
+  createBookfromItem(item: ReadingListItem, book: Book){
+    console.log("createBookfromItem : "+item.bookId)
+    book.id = item.bookId;
+    book.authors = item.authors;
+    book.description =  item.description;
+    book.coverUrl =  item.coverUrl;
+    book.publishedDate = item.publishedDate;
+    book.publisher =  item.publisher;
+    console.log("createBookfromItem : "+book.id)
+    
   }
 }
