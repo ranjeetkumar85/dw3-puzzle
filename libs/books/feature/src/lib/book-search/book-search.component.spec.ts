09b6ebd4bd30d6db1449b11b8dd5a0341214989c
ranjeet kumar <ranjeet.kumar85@t-mobile.com>
2021-06-15 22:56:20 -0700

    [chore]: Added Test cases

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,35 +1,80 @@
-import { FixedSizeVirtualScrollStrategy } from '@angular/cdk/scrolling';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { BOOKS } from './../constants/book-search-constants';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  tick,
+  fakeAsync,
+  discardPeriodicTasks
+} from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
-
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
-
-describe('ProductsListComponent', () => {
+import { provideMockStore } from '@ngrx/store/testing';
+import { clearSearch, getAllBooks, searchBooks } from '@tmo/books/data-access';
+import { Store } from '@ngrx/store';
+describe('BookSearchComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
-
+  let store: Store;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [
+        BooksFeatureModule,
+        NoopAnimationsModule,
+        SharedTestingModule
+      ],
+      providers: [
+        Store,
+        provideMockStore({
+          initialState: {
+            books: {
+              entities: []
+            }
+          },
+          selectors: [{
+            selector: getAllBooks,
+            value: []
+          }]
+        }),
+      ],
     }).compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(BookSearchComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
+    store = fixture.debugElement.injector.get(Store);
   });
 
-  it('should create', () => {
-    expect(component).toBeDefined();
-  });
+  describe('ngOnInit()', () => {
+    beforeEach(() => {
+      spyOn(component.searchForm, 'valueChanges');
+      spyOn(store, 'dispatch');
+      spyOn(store, 'select').and.returnValue(BOOKS);
+    });
 
-  it('Should be more than zero book when user search any term', () => {
-    component.searchForm.controls['term'].patchValue('angular');
-    fixture.detectChanges();
-    expect(fixture.debugElement.queryAll( By.css('book')).length).toBeGreaterThan(0);
-  });
+    it('It should call searchBooks() when we have value for search term and actual time spent after entering search term is equal to 500ms', fakeAsync(() => {
+      component.ngOnInit();
+      component.searchForm.controls.term.setValue('Angular');
+      tick(500);
+      component.searchForm.valueChanges.subscribe(() => {
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+        expect(store.dispatch).toHaveBeenCalledWith(searchBooks({ term: 'Angular' }))
+      });
+    }));
 
+    it('It should populate the array of Book object', fakeAsync(() => {
+      component.ngOnInit();
+      component.searchForm.controls.term.setValue('');
+      tick(400);
+      component.searchForm.valueChanges.subscribe(() => {
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+        expect(store.dispatch).toHaveBeenCalledWith(clearSearch())
+      });
+      expect(component.books$).toEqual(BOOKS);
+      discardPeriodicTasks();
+    }));
+  });
 });
---
+++ libs/books/feature/src/lib/constants/book-search-constants.ts
@@ -0,0 +1,10 @@
+/**
+ * books array with  mock values
+ */
+export const BOOKS = [{
+    isAdded: true,
+    id: '1001',
+    description: 'Angular book',
+    title:'Angular Beginners',
+    authors:['Kanithkar']
+  }];
