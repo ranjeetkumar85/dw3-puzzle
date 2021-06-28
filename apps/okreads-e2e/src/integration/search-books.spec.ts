0164c5705da3a4209d15a48056931d91150a5d65
ranjeet kumar <ranjeet.kumar85@t-mobile.com>
2021-06-28 13:18:43 -0700

    [chore]: test cases and formatting

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -1,14 +1,17 @@
-describe('When: I use the reading list feature', () => {
+describe('When: Use the search feature', () => {
   beforeEach(() => {
     cy.startAt('/');
   });
 
-  it('Then: I should see my reading list', () => {
-    cy.get('[data-testing="toggle-reading-list"]').click();
+  it('Then: I should be able to search books by title', () => {
+    cy.get('input[type="search"]').type('javascript');
 
-    cy.get('[data-testing="reading-list-container"]').should(
-      'contain.text',
-      'My Reading List'
-    );
+    cy.get('form').submit();
+
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
+  });
+
+  xit('Then: I should see search results as I am typing', () => {
+    // TODO: Implement this test!
   });
 });
