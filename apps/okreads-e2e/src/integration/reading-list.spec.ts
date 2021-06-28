29360ab1e1171b2e4d6ac171104df1b44688d6cd
ranjeet kumar <ranjeet.kumar85@t-mobile.com>
2021-06-28 01:24:12 -0700

    [chore]: corrected test cases

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -23,13 +23,6 @@ describe('When: I use the reading list feature', () => {
 
     cy.get('[data-testing="toggle-reading-list"]').click();
 
-    cy.get('[data-testing="reading-list-item"]').should('have.lengthOf.above', readingItemsCount);
-
-    cy.get('[data-testing="reading-list-container"]').should(
-      'contain.text',
-      'My Reading List'
-    );
-    
     cy.get('[data-testing="finish-book-cta"]:enabled').first().click();
 
     cy.get('[data-testing="finish-date"]').first().should('contain.text', 'Finished reading');
